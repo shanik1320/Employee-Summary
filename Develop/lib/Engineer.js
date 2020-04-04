fc3c86c310a20b43e841a6ba0591eebfb551c72d
shanik hernandez <shaniik.her93@gmail.com>
2020-04-03 22:21:25 -0400

    added classes to manager and engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, githubUsername) {
+        super(name, id, email)
+        this.githubUsername = githubUsername;
+    }
+    getGithub(){
+        return this.githubUsername
+    }
+    getRole(){
+        return "Engineer"
+    }
+}
+module.exports = Engineer
